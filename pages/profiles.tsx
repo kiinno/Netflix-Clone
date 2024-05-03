49a495e04e655caffc77e7b418ff5b8a0cdbdba4
kiinno <125929100+kiinno@users.noreply.github.com>
2024-05-03 21:06:52 +0300

    Update profiles.tsx

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -44,13 +44,13 @@ function Profiles() {
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
-  if (!session)
-    return {
-      redirect: {
-        destination: "/auth",
-        permanent: false,
-      },
-    };
+  //if (!session)
+   // return {
+    //  redirect: {
+     //   destination: "/auth",
+     //   permanent: false,
+   //   },
+//    };
   return {
     props: {},
   };
