dc023b00aeb80ebf095a3192a1ea06e3ee67bfb9
kiinno <125929100+kiinno@users.noreply.github.com>
2024-05-03 21:12:09 +0300

    Update current.ts

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -7,8 +7,8 @@ export default async function handler(
 ) {
   if (req.method !== "GET") return res.status(405).end();
   try {
-    const { currentUser } = await serverAuth(req, res);
-    res.status(200).json(currentUser);
+    // const { currentUser } = await serverAuth(req, res);
+    res.status(200).json({});
   } catch (error) {
     res.status(400).end();
   }
